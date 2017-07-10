f428302ba693c314051625feb8ded3a2458bee19
ChristinaGalani <christina.ch.galani@gmail.com>
2017-07-10 13:07:10 +0200

    created game logic

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 console.log("Up and running!");
-var cardOne = "queen" ;
-var cardTwo = "king";
-var cardThree= "queen";
-var cardFour = "king";
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped"+ " " + cardFour);
+var cards = ["queen","king","queen","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3]; 
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else {
+		alert("Sorry, try again...");
+	}
+
+}
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
