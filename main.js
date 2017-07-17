c8f902c9b6ec0eec1d96b78c606d5ad9842b60f7
ChristinaGalani <christina.ch.galani@gmail.com>
2017-07-17 19:55:30 +0200

    shuffling cards

--- main.js
+++ main.js
@@ -22,15 +22,39 @@ var cards = [
 		cardImage:"images/king-of-diamonds.png"
 	}
 ];
+
+var shuffle = function (array){
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+shuffle(cards);
+
 var cardsInPlay = [];
 
 var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 	}else {
-		alert("Sorry, try again...");
+		alert("Sorry, try again...");		
 	}
+	location.reload();	
 	
+	console.log(cards);
 };
 
 var flipCard = function (){
@@ -57,6 +81,8 @@ var createBoard = function (){
 };
 
 createBoard();
+
+
 
 
 
